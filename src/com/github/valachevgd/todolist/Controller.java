63dbecc4880a2f66889d5447cbd80b1317e87527
valchevgd <valchevgd@gmail.com>
2020-02-16 18:28:59 +0200

    load/save from txt file

--- src/com/github/valachevgd/todolist/Controller.java
+++ src/com/github/valachevgd/todolist/Controller.java
@@ -1,23 +1,15 @@
 package com.github.valachevgd.todolist;
 
+import com.github.valachevgd.todolist.datamodel.TodoData;
 import com.github.valachevgd.todolist.datamodel.TodoItem;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Controller {
-
-    private List<TodoItem> todoItems;
 
     @FXML
     private ListView<TodoItem> todoListView;
@@ -27,49 +19,19 @@ public class Controller {
     private Label deadlineLabel;
 
     public void initialize() {
-        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 62th birthday card for my dad",
-                LocalDate.of(2020, Month.FEBRUARY, 28));
 
-        TodoItem item2 = new TodoItem("Go to work", "Go to work and write some HTML and CSS",
-                LocalDate.of(2020, Month.FEBRUARY, 17));
-
-        TodoItem item3 = new TodoItem("Pay bills", "Pay a bill for water",
-                LocalDate.of(2020, Month.FEBRUARY, 15));
-
-        TodoItem item4 = new TodoItem("Learn Java", "Learn Java until the end of the year",
-                LocalDate.of(2020, Month.DECEMBER, 31));
-
-        TodoItem item5 = new TodoItem("Watch Champions League", "Watch games of Champions League returns this week",
-                LocalDate.of(2020, Month.FEBRUARY, 19));
-
-        todoItems = new ArrayList<TodoItem>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
-
-        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
-            @Override
-            public void changed(ObservableValue<? extends TodoItem> observableValue, TodoItem todoItem, TodoItem t1) {
-                if (t1 != null) {
-                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
-                    itemDetailsTextArea.setText(item.getDetails());
-                    DateTimeFormatter df = DateTimeFormatter.ofPattern("d MMMM, yyyy");
-                    deadlineLabel.setText(df.format(item.getDeadline()));
-                }
+        todoListView.getSelectionModel().selectedItemProperty().addListener((observableValue, todoItem, t1) -> {
+            if (t1 != null) {
+                TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                itemDetailsTextArea.setText(item.getDetails());
+                DateTimeFormatter df = DateTimeFormatter.ofPattern("d MMMM, yyyy");
+                deadlineLabel.setText(df.format(item.getDeadline()));
             }
         });
 
-        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
 
-    @FXML
-    public void handleClickListView() {
-        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
-        itemDetailsTextArea.setText(item.getDetails());
-        deadlineLabel.setText(item.getDeadline().toString());
-    }
 }
---
+++ src/com/github/valachevgd/todolist/datamodel/TodoData.java
@@ -0,0 +1,71 @@
+package com.github.valachevgd.todolist.datamodel;
+
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class TodoData {
+
+    private static TodoData instance = new TodoData();
+    private static String filename = "TodoItems.txt";
+
+    private List<TodoItem> todoItems;
+    private DateTimeFormatter formatter;
+
+    public static TodoData getInstance() {
+        return instance;
+    }
+
+    private TodoData() {
+        formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    }
+
+    public List<TodoItem> getTodoItems() {
+        return todoItems;
+    }
+
+    public void loadTodoItems() throws IOException {
+
+        todoItems = FXCollections.observableArrayList();
+        Path path = Paths.get(filename);
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
+            String input;
+            while ((input = bufferedReader.readLine()) != null) {
+                String[] itemPieces = input.split("\t");
+
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dateString = itemPieces[2];
+
+                LocalDate date = LocalDate.parse(dateString, formatter);
+
+                TodoItem todoItem = new TodoItem(shortDescription, details, date);
+                todoItems.add(todoItem);
+            }
+        }
+    }
+
+    public void storeTodoItems() throws IOException {
+
+        Path path = Paths.get(filename);
+
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path)) {
+            for (TodoItem item : todoItems) {
+                bufferedWriter.write(String.format("%s\t%s\t%s",
+                        item.getShortDescription(),
+                        item.getDetails(),
+                        item.getDeadline().format(formatter)));
+                bufferedWriter.newLine();
+            }
+        }
+    }
+}
